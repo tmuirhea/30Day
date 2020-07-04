29f3db6bd17bcdeb88c46bd78e6d779d1e9fa6f0
Junyang Li <jli328@ucsc.edu>
2020-07-03 17:34:58 -0700

    initial App.js code
    add features for the homepage(texts, buttons)

---
+++ App.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions } from 'react-native';
+import { Button } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.Title}>
+        <TouchableOpacity style={styles.selectButton}>
+          <Text style={styles.sb}> ≡ </Text>
+        </TouchableOpacity>
+        <Text style={styles.title}> 30 Days</Text>
+        <TouchableOpacity style={styles.plusButton}>
+          <Text style={styles.pb}> + </Text>
+        </TouchableOpacity>
+        </View>
+      <View style={styles.Yhnn}>
+        <Text style={styles.yhnn}> You have no new{"\n"} challenges,
+        click the +{"\n"} button to add</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  Yhnn:{
+    alignItems: 'center',
+    marginTop: width/2,
+
+  },
+  yhnn:{
+    fontSize:22,
+  },
+  Title:{
+    flexDirection:'row',
+    alignItems: 'center',
+    marginTop: 25,
+    marginLeft: 10,
+  },
+  title:{
+    fontSize:32,
+  },
+  fb:{
+    alignItems: 'center',
+    backgroundColor:'yellow',
+    marginHorizontal: 180,
+  },
+  pb:{
+    fontSize:32,
+  },
+  plusButton:{
+    marginLeft:width/2,
+  },
+  sb:{
+    fontSize:36,
+  }
+});
